b29e892019a23686b06227f1178d3d8e58bcd452
hidarr <91350265+hidarr@users.noreply.github.com>
2021-10-08 03:15:35 +0300

    Update functions.lua

--- inc/functions.lua
+++ inc/functions.lua
@@ -128,6 +128,7 @@ keyboardCmd = [[{
 "inline_keyboard": [
 [{"text": "①","callback_data": "List1"},{"text": "②","callback_data": "List2"},{"text": "③","callback_data": "List3"}],
 [{"text": "الالعاب","callback_data": "CmdSudo"},{"text": "Suorce","callback_data": "List4"}],
+{"text": "اوامر الردود","callback_data": "List5"}],
 [{"text": "","callback_data": "LoginOut"}]
 ]}
 ]]
