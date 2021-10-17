0c0c81a1e4b0bb9cff15f25ced1d064cdf11b126
hidarr <91350265+hidarr@users.noreply.github.com>
2021-10-17 21:42:58 +0300

    Update Script.lua

--- inc/Script.lua
+++ inc/Script.lua
@@ -4220,23 +4220,6 @@ end,{msg=msg})
 return false
 end
 
-if MsgText[1] == "الالعاب المتطوره" then
-local texs = [[  ‌‌‏‌‌‏‌‌‌‌‏                                    
-قائمه الالعاب المتطوره اضغط للعب
- ]]
- keyboard = {} 
- keyboard.inline_keyboard = {
- {
- {text = "شطرنج",url="https://t.me/T4TTTTBOT?game=chess"}, {text = "فلابي بيرد",url="https://t.me/awesomebot?game=FlappyBird"}, 
- },
- {
- {text = "رياضيات",url="https://t.me/gamebot?game=MathBattle"}, {text = "سباق سيارات",url="https://t.me/gamee?game=F1Racer"}, 
- },
- }
- 
-local msg_id = msg.id_/2097152/0.5
-https.request(ApiToken..'/sendMessage?chat_id=' .. msg.chat_id_ .. '&text=' .. URL.escape(texs).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
-end
 
 if MsgText[1] == "مغادره" or MsgText[1] == "ادفرني" or MsgText[1] == "احظرني" or MsgText[1] == "اطردني" then
 if msg.Admin then return "⤌لا استطيع طرد المدراء والادمنيه والمالكيين  \n" end
