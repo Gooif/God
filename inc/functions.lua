08f47206dc917fb77eb13d81ed46083a063ef0b1
Gooif <92914335+Gooif@users.noreply.github.com>
2021-11-08 20:50:52 +0200

    Update functions.lua

--- inc/functions.lua
+++ inc/functions.lua
@@ -128,8 +128,8 @@ keyboardCmd = [[{
 
 "inline_keyboard": [
 
-[{"text": "①","callback_data": "List1"},{"text": "②","callback_data": "List2"}],
-[{"text": "③","callback_data": "List3"}],
+[{"text": "م1","callback_data": "List1"},{"text": "م2","callback_data": "List2"}],
+[{"text": "م3","callback_data": "List3"}],
 [{"text": "الالعاب","callback_data": "CmdSudo"},{"text": "Suorce","callback_data": "List4"}],
 
 [{"text": "","callback_data": "LoginOut"}]
